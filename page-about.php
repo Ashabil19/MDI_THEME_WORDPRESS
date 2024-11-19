c2085ba933727fbbb4010f2d3b6eb9aa836859f2
Fikriwib12 <wibfikri@gmail.com>
2024-11-19 16:15:51 +0700

    update img hero about us

--- page-about.php
+++ page-about.php
@@ -7,7 +7,7 @@ get_header();
 
 <div class="container about-hero">
   <h5>Dengan pengalaman bertahun-tahun di bidang deteksi logam, kami memiliki pengetahuan yang mendalam tentang teknologi dan aplikasinya. Kami berkomitmen untuk terus menghadirkan inovasi terbaru dalam industri deteksi logam.</h5>
-  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/about_us_hero.svg" alt="logo">
+  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/about_us_hero2.svg" alt="logo">
   <h5>Dengan pengalaman bertahun-tahun di bidang deteksi logam, kami memiliki pengetahuan yang mendalam tentang teknologi dan aplikasinya. inovasi </h5>
   <div class="about-us">
     <h1>ABOUT</h1>
